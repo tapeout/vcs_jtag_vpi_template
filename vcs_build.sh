633061bddec61badc6bdf003a07b448dc4267638
Edward Wang <edward.c.wang@compdigitec.com>
2017-12-31 13:06:03 -0800

    Bundle jtag_vpi.v

--- vcs_build.sh
+++ vcs_build.sh
@@ -16,5 +16,5 @@ vcs -full64 +vcs+vcdpluson -debug_pp +v2k +vpi +define+VCS_VPI \
   +define+RANDOMIZE_REG_INIT +define+RANDOMIZE_MEM_INIT \
   +lint=all,noVCDE \
   -top vsim_top vsim_top.v \
-  jtag_vpi/jtag_vpi.v jtag_vpi_vcs.c -P jtag_vpi.tab \
+  jtag_vpi.v jtag_vpi_vcs.c -P jtag_vpi.tab \
   ../integration.v ../rocket-chip/vsim/generated-src/ee194.E300ArtyDevKitConfig.behav_srams.v
