6d6ce8e7762acf6b7d99df50d203d93eeaecd5ee
Jerry Zhao <jerryz123@berkeley.edu>
2019-04-30 14:02:21 -0700

    Fix for DigitalTop file naming change

--- vcs_build.sh
+++ vcs_build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CORE=boom
+
 # Need to define VCS_VPI so that the appropriate changes for VCS get baked in.
 # TODO: pass this in from VCS
 cat <<EOF > jtag_vpi_vcs.c
@@ -21,4 +23,4 @@ vcs -full64 +vcs+vcdpluson -debug_pp +v2k +vpi +define+VCS_VPI \
   -top vsim_top vsim_top.v \
   jtag_vpi.v jtag_vpi_vcs.c -P jtag_vpi.tab \
   sine.c -P sine.tab\
-  ../verilog/DigitalTop.v ../verilog/DigitalTop.behav_srams.v
+  ../verilog/DigitalTop.$CORE.v ../verilog/DigitalTop.$CORE.behav_srams.v
