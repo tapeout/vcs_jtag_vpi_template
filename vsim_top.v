bab55483d4cfaf76e7f6d755576eded537ec4cc0
jerryduan07 <jerry_duan@berkeley.edu>
2019-05-06 12:38:26 -0700

    modification to vsim_top.v (#6)

--- vsim_top.v
+++ vsim_top.v
@@ -24,15 +24,15 @@ module vsim_top();
   end
 
   always #ADC_SAMPLE_PERIOD begin
-    if (gfsk_started) begin
-      freq = (2250000.0 + gfsk_out * 62500.0) / 1000000000.0; // freq in Ghz so 1/freq is in ns
-      phase = phase + (2*PI*ADC_SAMPLE_PERIOD * freq);
-      if (phase > 2*PI) begin
-        phase = phase - 2*PI;
-      end
-      I <= 16 + 10 * $sine(phase);
-      Q <= 16 + 10 * $cosine(phase);
-    end else if (gfsk_out != 3'd0) begin
+    freq = (2250000.0 + gfsk_out * 62500.0) / 1000000000.0; // freq in Ghz so 1/freq is in ns
+    phase = phase + (2*PI*ADC_SAMPLE_PERIOD * freq);
+    if (phase > 2*PI) begin
+      phase = phase - 2*PI;
+    end
+    I <= 16 + 10 * $sine(phase);
+    Q <= 16 + 10 * $cosine(phase);
+    
+    if (gfsk_out != 3'd0) begin
       gfsk_started = 1'b1;
     end
   end
